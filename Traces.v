7ffa4769be2e433428af2ab09e0108e3777acd06
Karl Palmskog <palmskog@gmail.com>
2023-01-25 17:37:40 +0100

    follow ITree better with monotonicity and implicits

--- Traces.v
+++ Traces.v
@@ -1,8 +1,10 @@
+Set Implicit Arguments.
+Set Contextual Implicit.
 Set Primitive Projections.
 
 Section Trace.
 
-Variables A B : Type.
+Context {A B : Type}.
 
 Inductive traceF (X : Type) : Type :=
 | TnilF : A -> traceF X
@@ -12,12 +14,15 @@ CoInductive trace := gotrace { untrace : traceF trace }.
 
 End Trace.
 
+Arguments trace _ _ : clear implicits.
+Arguments traceF _ _ : clear implicits.
+
 Notation trace' A B := (traceF A B (trace A B)).
 
 Definition observe {A B} (tr : trace A B) : trace' A B := @untrace A B tr.
 
-Notation Tnil a := (gotrace _ _ (TnilF _ _ _ a)).
-Notation Tcons a b tr := (gotrace _ _ (TconsF _ _ _ a b tr)).
+Notation Tnil a := (gotrace (TnilF a)).
+Notation Tcons a b tr := (gotrace (TconsF a b tr)).
 
 Section Operations.
 
@@ -25,14 +30,14 @@ Variables A B : Type.
 
 Definition hd (tr : trace A B) : A :=
 match observe tr with
-| TnilF _ _ _ a => a
-| TconsF _ _ _ a b tr0 => a
+| TnilF a => a
+| TconsF a b tr0 => a
 end.
 
 CoFixpoint trace_append (tr tr' : trace A B) : trace A B :=
 match observe tr with
-| TnilF _ _ _ _ => tr'
-| TconsF _ _ _ a b tr0 => Tcons a b (trace_append tr' tr0)
+| TnilF _ => tr'
+| TconsF a b tr0 => Tcons a b (trace_append tr' tr0)
 end.
 
 #[local] Infix "+++" := trace_append (at level 60, right associativity).
@@ -48,15 +53,43 @@ Context {A B : Type}.
 Inductive eqtrF (sim : trace A B -> trace A B -> Prop) :
     trace' A B -> trace' A B -> Prop :=
 | EqTrTnilF a :
-   eqtrF sim (TnilF _ _ _ a) (TnilF _ _ _ a)
+   eqtrF sim (TnilF a) (TnilF a)
 | EqTrTconsF a b tr1 tr2 (REL : sim tr1 tr2) :
-   eqtrF sim (TconsF _ _ _ a b tr1) (TconsF _ _ _ a b tr2).
+  eqtrF sim (TconsF a b tr1) (TconsF a b tr2).
 
 Definition eqtr_ sim :
-    trace A B -> trace A B -> Prop :=
-    fun tr1 tr2 => eqtrF sim (observe tr1) (observe tr2).
+  trace A B -> trace A B -> Prop :=
+  fun tr1 tr2 => eqtrF sim (observe tr1) (observe tr2).
+
+Lemma eqtrF_mono sim sim' tr tr'
+ (IN: eqtrF sim tr tr')
+ (LE: sim <2= sim') :
+ eqtrF sim' tr tr'.
+Proof.
+intros. induction IN.
+- apply EqTrTnilF.
+- apply EqTrTconsF.
+  apply LE.
+  apply REL.
+Qed.
+
+Lemma eqtr__mono : monotone2 eqtr_.
+Proof.
+do 2 red. intros.
+eapply eqtrF_mono; eauto.
+Qed.
+
+Hint Resolve eqtr__mono : paco.
 
 Definition eqtr : trace A B -> trace A B -> Prop :=
-    paco2 eqtr_ bot2.
+  paco2 eqtr_ bot2.
+
+Inductive eqtr_trans_clo (r : trace A B -> trace A B -> Prop)
+  : trace A B -> trace A B -> Prop :=
+| eqtr_trans_clo_intro tr1 tr2 tr1' tr2'
+      (EQVl: eqtr tr1 tr1')
+      (EQVr: eqtr tr2 tr2')
+      (REL: r tr1' tr2')
+  : eqtr_trans_clo r tr1 tr2.
 
 End Eqtr.
